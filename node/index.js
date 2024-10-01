d3a682cfe453f974d1a4eb5ec5e6f4d4d3a22058
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 17:21:41 -0300

    chega

--- node/index.js
+++ node/index.js
@@ -1,18 +1,38 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-const port = 3000
+app.use(express.json());
+
+const port = 3000;
+const nome = "Fulano";
+let personList = [];
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send(nome);
+});
 
-  app.get('/batata', (req, res) => {
-    res.send('Batata!')
-  })
+app.get('/visualizar', (req, res) => {
+  res.send(personList);
+});
 
-  
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.post('/add', (req, res) => {
+  const { name, age } = req.body;
+  personList.push({ name, age });
+  res.send(`Usuário recebido, nome: ${name}`);
+});
+
+app.post('/re/:id', (req, res) => {
+  const { id } = req.params;
+  const index = parseInt(id, 10); 
+
+  if (index >= 0 && index < personList.length) {
+    const removedPerson = personList.splice(index, 1)[0]; 
+    res.send(`Usuário ${removedPerson.name} removido.`);
+  } else {
+    res.status(404).send('Usuário não encontrado.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
