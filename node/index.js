07d17824b691affe468005c165fd801a0b8ed866
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 14:47:11 -0300

    nodemom

--- node/index.js
+++ node/index.js
@@ -10,6 +10,8 @@ app.get('/', (req, res) => {
   app.get('/batata', (req, res) => {
     res.send('Batata!')
   })
+
+  
   
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
