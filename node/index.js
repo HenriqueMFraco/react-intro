15a920289cdc5fd33ba325df34a087667437f316
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-01 14:33:31 -0300

    batata

--- node/index.js
+++ node/index.js
@@ -6,6 +6,10 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+  app.get('/batata', (req, res) => {
+    res.send('Batata!')
+  })
   
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
